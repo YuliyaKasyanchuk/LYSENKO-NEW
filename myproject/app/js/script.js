3c1c759c2a4d9729848a263e933a56003c8058f1
Yulja <yuljakas@gmail.com>
2017-03-09 08:58:36 +0200

    script.js deleted from portfolio.html

--- myproject/app/js/script.js
+++ myproject/app/js/script.js
@@ -1,43 +1,6 @@
 $( document ).ready(function() {
-
-    jQuery('.item-masonry').hover(
-		function(){
-			$(this).find(".cover-item-gallery").fadeIn();
-		},
-		function(){
-			$(this).find(".cover-item-gallery").fadeOut();
-		}
-	);
-
-    var sizer = '.sizer4';
-
-    var container = $('#gallery');
-
-    container.imagesLoaded(function(){
-    	container.masonry({
-    		itemSelector: '.item-masonry',
-    		columnWidth: sizer,
-    		percentPosition: true
-		});
-
-
-
-        $('.hidden-menu li a').click(function () {
-
-        });
-
-
+    $('.hidden-menu li a').click(function () {
 
     });
-
-
-
-
-
-
-
-
-
-
 });
 
